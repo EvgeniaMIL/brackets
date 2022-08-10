0712e025157c4af1420788816e2dcba035797314
EvgeniaMIL <e.vaylet@mail.ru>
2022-08-10 13:11:20 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  // return str === str.split('').reverse().join('') 
+  // 9 из 20 метод полиндрома 
+
+for(let i = 0; i < str.length; i++) {
+  bracketsConfig.forEach( el => {
+    if(str[i] === el[0] && str[i + 1] === el[1]) {
+      str = str.slice(0, i) + str.slice(i + 2, str.length)
+      i -= 2
+    }
+  })
+}
+
+if (str.length === 0) {
+  return true;
+} else {
+  return false;
+}
+
 }
